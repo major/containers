aa47c9dfd08813f6a28b0317aca4a495a2329052
Major Hayden <major@mhtx.net>
2021-04-22 08:36:20 -0500

    Clean up RPMs right after installation

--- fedora-packager/build.sh
+++ fedora-packager/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -euxo pipefail
 
+# Install required packages and clean up.
 dnf -y install "@Fedora Packager" rpmdevtools vim
+dnf clean all
 
 # Allow kerberos to work from within a container.
 sed -i 's/default_ccache_name/d' /etc/krb5.conf
 
 # Set the default realm to make it easier to kinit.
 sed 's/^#.*default_realm.*$/    default_realm = FEDORAPROJECT.ORG/' /etc/krb5.conf
-
-dnf clean all
